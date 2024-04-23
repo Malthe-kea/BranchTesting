63de4441b35664ad3b5c0ced5466a102491f5d9f
malthe-kea <mati0002@stud.kea.dk>
2024-04-23 14:43:25 +0200

    Tilføjet to dyr, der siger noget

--- src/main.java
+++ src/main.java
@@ -1,9 +1,9 @@
 
 public class main {
     public static void main(String[] args) {
-        Dog bjarne = new Dog();
-        Cat flemming = new Cat();
-        bjarne.makeSound();
-        flemming.makeSound();
+        Dog jesus = new Dog();
+        Cat gud = new Cat();
+        gud.makeSound();
+        jesus.makeSound();
     }
 }
