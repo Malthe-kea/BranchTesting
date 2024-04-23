3f744604ef4a3dbb7950f7b812fa98a9c63c04d9
malthe-kea <mati0002@stud.kea.dk>
2024-04-23 14:34:16 +0200

    bjarne og flemming laver lyde nu

--- src/main.java
+++ src/main.java
@@ -1,6 +1,9 @@
+
 public class main {
     public static void main(String[] args) {
         Dog bjarne = new Dog();
+        Cat flemming = new Cat();
         bjarne.makeSound();
+        flemming.makeSound();
     }
 }
