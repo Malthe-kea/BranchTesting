192039e3c71b6cdaed12c1dcccb16bdeb03b94fe
malthe-kea <mati0002@stud.kea.dk>
2024-04-23 14:51:39 +0200

    pushed til git

--- src/main.java
+++ src/main.java
@@ -1,13 +1,13 @@
 
 public class main {
     public static void main(String[] args) {
-        Dog jesus = new Dog();
+        Dog jesuss = new Dog();
         Cat gud = new Cat();
         gud.makeSound();
-        jesus.makeSound();
+        jesuss.makeSound();
 
         gud.eat("Vin");
-        gud.eat("Kiks");
+        jesuss.eat("Kiks");
 
     }
 }
